e3af4e98c2a5f3e79db582d68b4c674443cd86dd
Aboogeeky <Idrishassan35@gmail.com>
2024-08-21 18:09:17 +0100

    Update Arrays&Structs.sol

--- Arrays&Structs.sol
+++ Arrays&Structs.sol
@@ -1,1 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
 
+contract SimpleStorage {
+    struct Animal {
+        uint256 myFavNumber;
+        string name;
+    }
+
+    Animal[] public listOfAnimals;
+
+    // Manually add three animals
+    constructor() {
+        addAnimal("Lion", 1);
+        addAnimal("Tiger", 2);
+        addAnimal("Bear", 3);
+    }
+
+    function addAnimal(string memory _name, uint256 _myFavNumber) public {
+        Animal memory newAnimal = Animal(_myFavNumber, _name);
+        listOfAnimals.push(newAnimal);
+    }
+}
